b5c8fb2396ea3c369277ebd6fc2ecd2189d306fd
limaarthur <artglima.dev@gmail.com>
2025-06-01 13:15:25 -0300

    feat: pages and routes - layout styling

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(100vh - 10rem);Add commentMore actions
+  margin: 5rem auto;
+  padding: 2.5rem;
+
+  background: ${(props) => props.theme['gray-800']};Add commentMore actions
+  border-radius: 8px;
+
+  display: flex;Add commentMore actions
+  flex-direction: column;
+`
