dfb496e72966543b39d561f6e2ee987dcc519580
limaarthur <artglima.dev@gmail.com>
2025-06-02 23:55:09 -0300

    feat: pages and routes - creating header component

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
-  height: calc(100vh - 10rem);Add commentMore actions
+  height: calc(100vh - 10rem);
   margin: 5rem auto;
   padding: 2.5rem;
 
-  background: ${(props) => props.theme['gray-800']};Add commentMore actions
+  background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
 
-  display: flex;Add commentMore actions
+  display: flex;
   flex-direction: column;
 `
