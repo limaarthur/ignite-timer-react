3eb10ab1e6651b1f96ec5de5154a5d911beaa289
limaarthur <artglima.dev@gmail.com>
2025-06-03 17:25:15 -0300

    feat: pages and routes - home finalization

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,10 @@ import { Play } from 'phosphor-react'
 import {
   HomeContainer,
   FormContainer,
+  TaskInput,
+  MinutesAmountInput,
   CountdownContainer,
+  StartCountdownButton,
   Separator,
 } from './styles'
 
@@ -12,10 +15,14 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -28,13 +35,11 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
 }
-
-export default Home
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -29,6 +29,35 @@ export const FormContainer = styled.main`
   font-weight: bold;
   flex-wrap: wrap;
 `
+
+export const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
 export const CountdownContainer = styled.main`
   font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
@@ -44,6 +73,35 @@ export const CountdownContainer = styled.main`
     border-radius: 8px;
   }
 `
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`
+
 export const Separator = styled.main`
   display: flex;
   justify-content: center;
