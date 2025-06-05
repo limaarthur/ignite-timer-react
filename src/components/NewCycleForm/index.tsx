6eaf53d558774082ba747eee9eba0bde127ebcde
limaarthur <artglima.dev@gmail.com>
2025-06-05 16:29:25 -0300

    feat: context in react - context between routes

--- src/components/NewCycleForm/index.tsx
+++ src/components/NewCycleForm/index.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { useFormContext } from 'react-hook-form'
-import { CyclesContext } from '../../pages/Home'
+import { CyclesContext } from '../../contexts/CyclesContext'
 
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
 
